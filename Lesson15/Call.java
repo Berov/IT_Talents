0afc3511473126eb0f50292192192a39b5818a0f
Berov <nikolai.berov@gmail.com>
2017-01-16 11:18:50 +0200

    OOP - Lesson 15

--- Lesson15/Call.java
+++ Lesson15/Call.java
@@ -1,4 +1,13 @@
 
 public class Call {
+	// priceForAMinute – статично поле, което показва цената на разговор за минута
+	// caller - показва телефона, от който е направено повикването
+	// receiver - показва телефона, към който е направено повикването
+	// duration - дължината на разговора (в минути)
+
 	double priceForAMinute = 2.5;
+	String caller;
+	String reciever;
+	double duration;
+
 }
